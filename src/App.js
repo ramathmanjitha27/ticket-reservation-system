588e3fdd9cb4812f17f41f2da05505d7a46501d5
Ramath <ramathmanjitha@gmail.com>
2023-10-10 11:04:38 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CreateTrain from "./shared/screens/train-managment/create-train";
+import { Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row>
+        <Col>
+          <CreateTrain />
+        </Col>
+      </Row>
+    </>
   );
 }
 
